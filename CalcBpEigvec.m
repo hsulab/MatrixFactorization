6d6ea6e1c678d059529a06274a1a2584034c5429
许嘉琰 <ahcigar@foxmail.com>
2019-04-22 19:33:11 +0800

    Add documentation.

--- CalcBpEigvec.m
+++ CalcBpEigvec.m
@@ -1,22 +1,27 @@
 #!/usr/local/bin/octave
-#####################################################
-### Octave is a free software similiar to MATLAB. ###
-#####################################################
 function [w] = CalcBpEigvec(Bp)
-    % Bp
-    a = Bp(1,1); b = Bp(1,2);
-    c = Bp(2,1); d = Bp(2,2);
+% CalcBpEigvec computes the dominant eigenvector of 2x2 matrix.
+% IN:
+%   Bp - 2x2 matrix
+% OUT:
+%   w - eigenvector of Bp
+% Usage:
+%   [w] = CalcBpEigvec(Bp)
 
-    % lambda^2-(a+d)*lambda+ad-bc=0
-    delta = (a+d)^2-4*(a*d-b*c);
+% get entries of Bp
+a = Bp(1,1); b = Bp(1,2);
+c = Bp(2,1); d = Bp(2,2);
 
-    % Find w, eigenvector of smallest eigenvalue of Bp
-    lambda = ((a+d)-sqrt(delta))/2;
+% lambda^2-(a+d)*lambda+ad-bc=0
+delta = (a+d)^2-4*(a*d-b*c);
 
-    % w = [x1;x2] set x1=1
-    x2 = (a+c-lambda)/(lambda-2*b);
-    w = [1;x2];
-    w = w/norm(w);
+% find w, eigenvector of smallest eigenvalue of Bp
+lambda = ((a+d)-sqrt(delta))/2;
+
+% w = [x1;x2] set x1=1
+x2 = (a+c-lambda)/(lambda-2*b);
+w = [1;x2];
+w = w/norm(w);
 
 end
 
