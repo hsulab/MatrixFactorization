292b044db7d5f29d3f1856e448fb5c7017cb60cd
许嘉琰 <ahcigar@foxmail.com>
2019-04-24 09:46:48 +0800

    Add Documentation.

--- HornerPoly.m
+++ HornerPoly.m
@@ -1,22 +1,25 @@
 #!/usr/local/bin/octave
-#####################################################
-### Octave is a free software similiar to MATLAB. ###
-#####################################################
 function polyvalue = HornerPoly(A,x)
-    % Horner's Method is fast to calculate polynomial value.
+% Horner's Method is fast to calculate polynomial value.
+% IN:
+%   A - coefficients
+%   x - value
+% OUT:
+%
+% Usage:
+%   polyvalue = HornerPoly(A,x)
 
-    % Set the initial value as 0.
-    polyvalue = 0;
+% Set the initial value as 0.
+polyvalue = 0;
 
-    % Determine the degree of coefficient matrix A.
-    % a_n*x^n + a_(n-1)*x^(n-1) + ... + a0.
-    % A = [a_n, a_(n-1), ..., a0].
-    d = length(A);
+% Determine the degree of coefficient matrix A.
+% a_n*x^n + a_(n-1)*x^(n-1) + ... + a0.
+% A = [a_n, a_(n-1), ..., a0].
+d = length(A);
 
-    % Calculate the value of the polynomial.
-    for i=1:d
-        polyvalue = A(i)+polyvalue*x;
-    end
-
+% Calculate the value of the polynomial.
+for i=1:d
+    polyvalue = A(i)+polyvalue*x;
 end
 
+end
